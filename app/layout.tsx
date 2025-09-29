6ab223d787d8460ec9fca0d220737722e626e796
Ankit Vishwakarma <ankvish1102@gmail.com>
2025-09-29 14:40:05 +0530

    Implement Header

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Signalist",
+  title: "MarketPulse - Real-Time Stock Tracker",
   description: "Track real-time stock prices, get personalized alerts and explore detailed company insights.",
 };
 
